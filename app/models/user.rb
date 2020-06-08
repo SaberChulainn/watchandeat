f3cd5fbd7699a676880e30f209bfd9976e27f89e
SaberChulainn <Tbandachit@gmail.com>
2020-06-08 15:22:26 -0700

    auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :username, presence: true, uniqueness: true
+    has_secure_password
     validates :password, presence: true, length: { minimum: 5 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :movie_pairs
-  resources :foods
-  resources :movies
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+      resources :movie_pairs
+      resources :foods
+      resources :movies
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
