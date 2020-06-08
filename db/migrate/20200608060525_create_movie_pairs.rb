094245f6763b696612acf57cd0f88ba5766fd248
SaberChulainn <Tbandachit@gmail.com>
2020-06-07 23:10:28 -0700

    database added

--- db/migrate/20200608060525_create_movie_pairs.rb
+++ db/migrate/20200608060525_create_movie_pairs.rb
@@ -1,7 +1,8 @@
 class CreateMoviePairs < ActiveRecord::Migration[6.0]
   def change
     create_table :movie_pairs do |t|
-
+      t.integer :movie_id
+      t.integer :food_id
       t.timestamps
     end
   end
